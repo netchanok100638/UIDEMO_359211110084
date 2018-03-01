7278f420500b9bc31c28c0d1c3e217dfa382435e
Netchanok100638 <Netchanok.pk2535@gmail.com>
2018-03-01 15:43:18 +0700

    dbConnection

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,16 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            primaryStage.setTitle("School Management System");
+            primaryStage.setScene(new Scene(root, 600, 390));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        }
 
 
-    public static void main(String[] args) {
-        launch(args);
+        public static void main(String[] args) {
+            launch(args);
+        }
     }
-}
